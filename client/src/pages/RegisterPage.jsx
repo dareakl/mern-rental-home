9bdfb69bbe1feb5224a50f0afcab5833df486326
Sathish Raja <msathis.akl@hotmail.com>
2024-12-21 13:33:23 +0800

    register form handled function added

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,38 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Register.scss";
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profileImage: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+
+    if (name === "profileImage") {
+      setFormData({
+        ...formData,
+        profileImage: files[0], // Set the selected file
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value, // Update other input fields
+      });
+    }
+  };
+
+  console.log(formData);
+
   return (
     <div className="register">
       <div className="register_content">
-        <form>
-          <input placeholder="First Name" name="firstname" required />
-          <input placeholder="Last Name" name="lastname" required />
-          <input placeholder="Email" name="email" type="email" required />
+        <form className="register_content_form">
+          <input
+            placeholder="First Name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            placeholder="Last Name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            type="email"
+            onChange={handleChange}
+            required
+          />
           <input
             placeholder="Password"
             name="password"
+            value={formData.password}
             type="password"
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Confirm password"
             name="confirmPassword"
+            value={formData.confirmPassword}
             type="password"
+            onChange={handleChange}
             required
           />
+          {/* Don't bind value to the file input */}
           <input
             id="image"
             type="file"
             name="profileImage"
             accept="image/*"
             style={{ display: "none" }}
+            onChange={handleChange}
             required
           />
           <label htmlFor="image">
             <img src="/assets/addImage.png" alt="add profile photo" />
             <p>Upload Your Photo</p>
           </label>
+          {formData.profileImage && (
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile photo"
+              style={{ maxWidth: "80px" }}
+            />
+          )}
           <button type="submit">REGISTER</button>
         </form>
         <a href="/login">Already have an account? Log In Here</a>
